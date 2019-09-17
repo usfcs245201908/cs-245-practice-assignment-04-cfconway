f8b7e6eb38004a26726637f6a0ccb108bb2f0e24
cfconway <47462782+cfconway@users.noreply.github.com>
2019-09-17 15:09:55 -0700

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,29 @@
+public class SelectionSort implements SortingAlgorithm
+{
+	public void sort(int [] a)
+	{	for(int i=0; i<a.length;i++)
+			swap(a,i,findMin(a,i));
+
+	}
+
+	public void swap(int [] a, int x, int y)
+	{
+		int temp=a[x];
+		a[x]=a[y];
+		a[y]=temp;
+	}
+
+	public int findMin(int [] a, int start)
+	{
+		int min = start;
+		for(int i=start;i<a.length;i++)
+		{
+			if(a[i]<a[min])
+			{
+				min=i;
+			}
+
+		} return min;
+	}
+	
+}
